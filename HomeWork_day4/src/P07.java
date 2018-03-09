0dd6240504f227a76c2341832a15a2f902957d90
Maksym Novakh <novakhm@gmail.com>
2018-03-09 09:22:34 -0500

    modified P07 and passed tests

--- HomeWork_day4/src/P07.java
+++ HomeWork_day4/src/P07.java
@@ -6,16 +6,24 @@ import java.util.List;
  * Created by student on 3/8/18.
  */
 public class P07 {
-    private List<String> myList;
+    private List<String> myList = new ArrayList<>();
 
     public List<String> flattern(List<List<String>> list) {
-        Iterator it = list.iterator();
-        while(it.hasNext()) {
-            if (it.next() instanceof String) {
-                myList.add((String)it.next());
-            };
-            return flattern((List)it.next());
-        };
+        Iterator<List<String>> it = list.iterator();
+        while (it.hasNext()) {
+            List<String> value = it.next();
+            flattenString(value);
+        }
+        ;
         return myList;
+    };
+
+    public void flattenString(List<String> list) {
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            String str = iterator.next();
+            myList.add(str);
+        }
+        ;
     };
 }
--- HomeWork_day4/src/TestP07.java
+++ HomeWork_day4/src/TestP07.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
@@ -11,8 +12,23 @@ public class TestP07 {
     @Test
     public void testP07() {
         P07 instance = new P07();
-        List<List<String>> listOfAs = asList("a", asList("b", "m", asList("c", "o"), asList("d", "e", asList("f", "g"), "a")), "h");
+        List<String> list1 = new ArrayList<>();
+        list1.add("Dude");
+        list1.add("Order");
+        list1.add("Never");
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("D");
+        list2.add("O");
+
+
+
+
+        List<List<String>> listOfAs = new ArrayList<>();
+        listOfAs.add(list1);
+        listOfAs.add(list2);
+
         List<String> killo = instance.flattern(listOfAs);
-        assertEquals(6, killo.size());
+        assertEquals(5, killo.size());
     }
 }
